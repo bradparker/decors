0823e3a9f32c8e4a414ff7b0c792e97080831434
Brad Parker <hi@bradparker.com>
2015-01-24 17:22:05 +1000

    Get correct port in prod

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', function(req, res, next) {
   request(req.query.url).pipe(res);
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
   var host = server.address().address
   var port = server.address().port
